c82ffcb1b922f072a782285dd1e0de6fafc583d9
Jon Grythe Stødle <jon@stodle.net>
2021-10-12 20:58:08 +0200

    Move input to own component

---
+++ src/lib/TodoInput.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	import { generateId } from '$lib/utils';
+
+	let todoText = "";
+
+	const addTodo = () => {
+		todos = [...todos, {
+			id: generateId(),
+			text: todoText,
+			done: false,
+		}];
+		todoText = "";
+	}
+</script>
+
+<input type='text' bind:value={todoText} on:keydown={(e) => e.key == "Enter" && addTodo()}>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,15 @@
 <script lang='ts'>
-	import {generateId} from '$lib/utils';
 	import TodosList from '$lib/TodosList.svelte';
+	import TodoInput from '$lib/TodoInput.svelte';
 
-	let todoText = "";
 	let todos = [];
 
 	$: todosNotDone = todos
 		.filter(t => !t.done)
-
-	const addTodo = () => {
-		todos = [...todos, {
-			id: generateId(),
-			text: todoText,
-			done: false,
-		}];
-		todoText = "";
-	}
 </script>
 
 <h1>{todosNotDone.length} gjenstående</h1>
 
-<input type='text' bind:value={todoText} on:keydown={(e) => e.key == "Enter" && addTodo()}>
+<TodoInput />
 
 <TodosList {todos} />
